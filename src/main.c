dced26818dfbe1af8e214a81ce6cd1e8b09ccb26
sutalite <user@sutalite.me>
2021-02-06 13:46:13 +0100

    Use union instead of bit manipulation for accumulator

--- src/main.c
+++ src/main.c
@@ -14,7 +14,13 @@ typedef enum {
 } register_type;
 
 typedef struct {
-    uint16_t d;
+    union {
+        struct {
+            uint8_t a;
+            uint8_t b;
+        };
+        uint16_t d;
+    };
     uint16_t sp;
     uint16_t pc;
     uint8_t status;
@@ -27,15 +33,15 @@ const char *register_name(register_type rt) {
     case ACC_A: return "ACC A";
     case ACC_B: return "ACC B";
     case ACC_D: return "ACC D";
-    default: return "Undetermined";
+    default: return "Undefined";
     }
 }
 
 uint16_t read_register(cpu *cpu, register_type rt) {
     uint16_t value = 0;
     switch (rt) {
-    case ACC_A: value = cpu->d & 0xFF00; break;
-    case ACC_B: value = cpu->d & 0x00FF; break;
+    case ACC_A: value = cpu->a; break;
+    case ACC_B: value = cpu->b; break;
     case ACC_D: value = cpu->d; break;
     default: break;
     }
@@ -46,15 +52,13 @@ uint16_t read_register(cpu *cpu, register_type rt) {
 void write_register(cpu *cpu, register_type rt, uint16_t value) {
     // Only keep the highest and lowest 8 bits for acc_a and acc_b respectively.
     // Keep the value as it is for acc_d.
-    uint16_t result = 0;
+    printf("Writing %s: %04x\n", register_name(rt), value);
     switch (rt) {
-    case ACC_A: result = (cpu->d & 0x00FF) | (value << 8); break;
-    case ACC_B: result = (cpu->d & 0xFF00) | (value & 0x00FF) ; break;
-    case ACC_D: result = value; break;
+    case ACC_A: cpu->a = value; break;
+    case ACC_B: cpu->b = value; break;
+    case ACC_D: cpu->d = value; break;
     default: break;
     }
-    printf("Writing %s: %04x\n", register_name(rt), result);
-    cpu->d = result;
 }
 
 void write_memory(cpu *cpu, uint16_t pos, uint8_t value) {
@@ -72,8 +76,8 @@ int main() {
     cpu cpu = {0};
 
     printf("Registers\n");
-    write_register(&cpu, ACC_B, 0x66);
     write_register(&cpu, ACC_A, 0x33);
+    write_register(&cpu, ACC_B, 0x66);
     read_register(&cpu, ACC_A);
     read_register(&cpu, ACC_B);
     read_register(&cpu, ACC_D);
