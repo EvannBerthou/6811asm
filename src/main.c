c06d59eedabd2954f082cf06cc62f5752014b675
Sutalite <evann.berthou@gmail.com>
2021-04-10 19:10:28 +0200

    Dump memory directly to a file with optional readable format

--- src/main.c
+++ src/main.c
@@ -11,6 +11,17 @@
 #define FMT8 "0x%02x"
 #define FMT16 "0x%04x"
 
+typedef struct {
+    struct {
+        uint8_t step          : 1;
+        uint8_t dump          : 1;
+        uint8_t from_dump     : 1;
+        uint8_t readable_dump : 1;
+        uint8_t print_info    : 1;
+    };
+    char *dump_path;
+} args;
+
 typedef enum {
     CARRY = 0x1,
     OFLOW = 0x2,
@@ -1430,42 +1441,71 @@ void exec_program(cpu *cpu, int step) {
     }
 }
 
-void dump_program(const char *file_name) {
-    cpu c = (cpu) {0};
-    if (!load_program(&c, file_name)) {
-        return;
+void dump_memory(const cpu *c, args *args) {
+    FILE *output_file;
+    if (args->dump_path == NULL) {
+        output_file = stdout;
+    } else {
+        output_file = fopen(args->dump_path, "w");
+        if (!output_file) {
+            fprintf(stderr, "Error while opening file %s\n", args->dump_path);
+            exit(1);
+        }
     }
+
     for (size_t i = 0; i < MAX_MEMORY; ++i) {
-        if (i % 16 == 0 && i != 0) printf("\n");
-        printf(FMT8" ", c.memory[i]);
+        // Only print newline when the "readable dump" argument has been given
+        if (args->readable_dump && i % 16 == 0 && i != 0) {
+            fprintf(output_file, "\n");
+        }
+        fprintf(output_file, FMT8" ", c->memory[i]);
+    }
+}
+
+void handle_args(args *args, int argc, char **argv) {
+    // Skip the first argument which is the program name itself
+    argc--;
+    argv++;
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "-step") == 0) {
+            args->step = 1;
+        }
+        else if (strcmp(argv[i], "-dump") == 0) {
+            args->dump = 1;
+            if (i + 1 < argc && argv[i + 1][0] != '-') {
+                args->dump_path = argv[i + 1];
+                ++i;
+            }
+        }
+        else if (strcmp(argv[i], "-readable") == 0) {
+            args->readable_dump = 1;
+        } else {
+            fprintf(stderr, "Unknow argument `%s`\n", argv[i]);
+        }
     }
 }
 
 int main(int argc, char **argv) {
-    int step = 0;
-    for (int i = 0; i < argc; ++i) {
-        if (strcmp(argv[i], "step") == 0) {
-            step = 1;
-        }
-        else if (strcmp(argv[i], "dump") == 0) {
-            dump_program(file_name);
-            exit(1);
-        }
-    }
+    args args = {0};
+    handle_args(&args, argc, argv);
 
     add_instructions_func();
 
     cpu c = (cpu) {0};
     set_default_ddr(&c);
 
-    INFO("%s", "Loading program");
+    //INFO("%s", "Loading program");
     if (!load_program(&c, file_name)) {
         return 0;
     }
 
-    INFO("%s", "Loading sucess");
-    INFO("%s", "Execution program");
-    exec_program(&c, step);
-    INFO("%s", "Execution ended");
+    if (args.dump) {
+        dump_memory(&c, &args);
+    } else {
+        //INFO("%s", "Loading sucess");
+        //INFO("%s", "Execution program");
+        exec_program(&c, args.step);
+        //INFO("%s", "Execution ended");
+    }
 }
 
