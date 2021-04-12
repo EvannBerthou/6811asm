bbcc91514f1a6dffe764c205a0dabf8ed5201b48
Sutalite <evann.berthou@gmail.com>
2021-04-12 17:56:18 +0200

    Moved test implementation in an other file

--- tests/main.c
+++ tests/main.c
@@ -1,29 +1,5 @@
 #include "../src/emulator.h"
-
-char *current_test;
-int test_n = 1;
-
-#define test_fmt "TEST '%s' (%d) "
-#define test_args current_test, test_n
-#define TEST_FAILED test_fmt"FAILED\n", test_args
-#define TEST_SUCCESS test_fmt"SUCCESS\n", test_args
-#define CRIT_TEST_FAILED "[CRITICAL] "test_fmt"FAILED\n", test_args
-
-#define TEST(name) {printf("\n");current_test = (name); test_n = 1;}
-
-#define ASSERT_EQ(x,y) test_n++; if ((x) != (y)) { printf(TEST_FAILED); printf("    Expected: '%d'. Recieved: '%d'\n", x,y); } else { printf(TEST_SUCCESS);}
-
-#define ASSERT_NEQ(x,y) test_n++; if ((x) == (y)) { printf(TEST_FAILED); printf("    Expected value different to '%d'\n", x); } else { printf(TEST_SUCCESS);}
-
-#define ASSERT(x) test_n++; if (!(x)) { printf(TEST_FAILED); printf("    Expected: TRUE. Recieved: FALSE\n"); } else { printf(TEST_SUCCESS);}
-
-
-#define CRIT_ASSERT_EQ(x,y) test_n++; if ((x) != (y)) { printf(CRIT_TEST_FAILED); printf("    Expected: '%d'. Recieved: '%d'\n", x,y); exit(0);} else { printf(TEST_SUCCESS);}
-
-#define CRIT_ASSERT_NEQ(x,y) test_n++; if ((x) == (y)) { printf(CRIT_TEST_FAILED); printf("    Expected value different to '%d'\n", x); exit(0);} else { printf(TEST_SUCCESS);}
-
-#define CRIT_ASSERT(x) test_n++; if (!(x)) { printf(CRIT_TEST_FAILED); printf("    Expected: TRUE. Recieved: FALSE\n");exit(0); } else { printf(TEST_SUCCESS);}
-
+#include "tests.h"
 
 void init(cpu *c) {
     add_instructions_func();
---
+++ tests/tests.h
@@ -0,0 +1,28 @@
+#ifndef TEST_H
+#define TEST_H
+
+char *current_test;
+int test_n = 0;
+
+#define test_fmt "TEST '%s' (%d) "
+#define test_args current_test, test_n
+#define TEST_FAILED test_fmt"\x1b[1;30;41mFAILED\x1b[m\n", test_args
+#define TEST_SUCCESS test_fmt"\x1b[1;30;42mSUCCESS\x1b[m\n", test_args
+#define CRIT_TEST_FAILED "[CRITICAL] "TEST_FAILED
+
+#define TEST(name) {printf("\n");current_test = (name); test_n = 0;}
+
+#define ASSERT_EQ(x,y) test_n++; if ((x) != (y)) { printf(TEST_FAILED); printf("    Expected: '%d'. Recieved: '%d'\n", x,y); } else { printf(TEST_SUCCESS);}
+
+#define ASSERT_NEQ(x,y) test_n++; if ((x) == (y)) { printf(TEST_FAILED); printf("    Expected value different to '%d'\n", x); } else { printf(TEST_SUCCESS);}
+
+#define ASSERT(x) test_n++; if (!(x)) { printf(TEST_FAILED); printf("    Expected: TRUE. Recieved: FALSE\n"); } else { printf(TEST_SUCCESS);}
+
+
+#define CRIT_ASSERT_EQ(x,y) test_n++; if ((x) != (y)) { printf(CRIT_TEST_FAILED); printf("    Expected: '%d'. Recieved: '%d'\n", x,y); exit(0);} else { printf(TEST_SUCCESS);}
+
+#define CRIT_ASSERT_NEQ(x,y) test_n++; if ((x) == (y)) { printf(CRIT_TEST_FAILED); printf("    Expected value different to '%d'\n", x); exit(0);} else { printf(TEST_SUCCESS);}
+
+#define CRIT_ASSERT(x) test_n++; if (!(x)) { printf(CRIT_TEST_FAILED); printf("    Expected: TRUE. Recieved: FALSE\n");exit(0); } else { printf(TEST_SUCCESS);}
+
+#endif
