9c63cf0428367d2ce73ac70b7fef566a8157dc02
Sutalite <evann.berthou@gmail.com>
2021-05-08 15:30:13 +0200

    Added tests for LSR and LSL

--- tests/main.c
+++ tests/main.c
@@ -1,11 +1,9 @@
+#define EMULATOR_IMPLEMENTATION
 #include "../src/emulator.h"
 #include "tests.h"
 
-void init(cpu *c) {
-    add_instructions_func();
-    if (!load_program(c, file_name)) {
-        return;
-    }
+void exec_instr(cpu *cpu, int opcode) {
+    (*instr_func[opcode])(cpu);
 }
 
 int cmp_mnemonic(mnemonic *m1, mnemonic *m2) {
@@ -15,23 +13,24 @@ int cmp_mnemonic(mnemonic *m1, mnemonic *m2) {
 }
 
 int main() {
-    cpu c = (cpu) {0};
-    init(&c);
+    cpu cpu = {0};
+    add_instructions_func();
 
+    /*
     TEST ("Mneominic parsing") {
         char s1[] = " lda #$FF";
         mnemonic m1 = line_to_mnemonic(s1, NULL, 0, 0);
-        mnemonic e1 = {0x86, {0xFF, IMMEDIATE, 0}};
+        mnemonic e1 = {0x86, {0xFF, IMMEDIATE, 0}, 0};
         ASSERT(cmp_mnemonic(&m1, &e1));
 
         char s2[] = " lda $FF";
         mnemonic m2 = line_to_mnemonic(s2, NULL, 0, 0);
-        mnemonic e2 = {0xB6, {0xFF, EXTENDED, 0}};
+        mnemonic e2 = {0xB6, {0xFF, EXTENDED, 0}, 0};
         ASSERT(cmp_mnemonic(&m2, &e2));
 
         char s3[] = " lda <$FF";
         mnemonic m3 = line_to_mnemonic(s3, NULL, 0, 0);
-        mnemonic e3 = {0x96, {0xFF, DIRECT, 0}};
+        mnemonic e3 = {0x96, {0xFF, DIRECT, 0}, 0};
         ASSERT(cmp_mnemonic(&m3, &e3));
 
         char s4[] = " lda <$FF";
@@ -43,7 +42,20 @@ int main() {
         mnemonic m5 = line_to_mnemonic(s5, NULL, 0, 0);
         mnemonic e5 = {0x39, {0, INHERENT, 0}};
         ASSERT(cmp_mnemonic(&m5, &e5));
+    }*/
 
+    TEST ("LSL and LSR tests") {
+        cpu.a = 0xFF;
+        mnemonic m = line_to_mnemonic((char[]){" lsla"}, NULL, 0, 0);
+        exec_instr(&cpu, m.opcode);
+        ASSERT_EQ(cpu.a, ((0xFF << 1) & 0xFF));
+        ASSERT_EQ(cpu.c, 1);
+
+        cpu.d = 0xFFF0;
+        m = line_to_mnemonic((char[]){" lsrd"}, NULL, 0, 0);
+        exec_instr(&cpu, m.opcode);
+        ASSERT_EQ(cpu.d, (0xFFF0 >> 1));
+        ASSERT_EQ(cpu.c, 0);
     }
 
     return 0;
